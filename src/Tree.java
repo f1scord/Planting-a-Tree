74dd517bfc898c83c2d9c314aa1fe4a94cbd3fac
f1scord <toha.ovcharenko@gmail.com>
2024-07-14 00:35:13 +0200

    First java project commit

---
+++ src/Tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args) {
+        // Introducing yourself and printing a tree to the screen
+        System.out.println("Hey there, I’m Anton and I’m learning to code in Java!");
+        System.out.println("I’m going to plant a tree today!");
+
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
